027df4e5b2c7503a66c01de3da0cf746cda7fc88
rodrigocunh4 <r-cunha@outlook.com>
2016-05-14 14:27:21 -0300

    Cadastro de professores e alunos

--- server/app/database/AlunoDatabase.java
+++ server/app/database/AlunoDatabase.java
@@ -5,6 +5,7 @@ import java.util.List;
 import play.db.jpa.JPA;
 import play.db.jpa.Transactional;
 import models.Aluno;
+import models.Professor;
 
 public class AlunoDatabase {
 	
@@ -22,10 +23,33 @@ public class AlunoDatabase {
 	}
 	
 	@Transactional
+	public static Aluno selectAluno(String email, String cnpjInst)throws Exception{
+		String query = "FROM Aluno WHERE email = :email AND cnpjInst = :cnpjInst";
+		List<Aluno> la = JPA.em().createQuery(query)
+								.setParameter("email", email)
+								.setParameter("cnpjInst", cnpjInst)
+								.getResultList();
+		if(la.isEmpty()){
+			return null;
+		}else{
+			return la.get(0);
+		}
+	}
+	
+	@Transactional
 	public static List<Aluno> selectAluno()throws Exception{
 		String query = "FROM Aluno";
 		List<Aluno> li = JPA.em().createQuery(query)
 								.getResultList();
 		return li;
+	}
+	
+	@Transactional
+	public static List<Aluno> selectAlunoByCnpjInst(String cnpjInst)throws Exception{
+		String query = "FROM Aluno WHERE cnpjInst = :cnpjInst";
+		List<Aluno> lp = JPA.em().createQuery(query)
+								.setParameter("cnpjInst", cnpjInst)
+								.getResultList();
+		return lp;
 	}
 }
