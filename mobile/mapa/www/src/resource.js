6440f78414a6871ddc3c0e20f9e80b902d66af02
daniellnog <daniellnog@gmail.com>
2016-05-22 12:08:53 -0300

    Adicionando uma pasta para os assets do mapa

--- mobile/mapa/www/src/resource.js
+++ mobile/mapa/www/src/resource.js
@@ -1,19 +1,19 @@
 var asset = {
-    HelloWorld_png : "asset/HelloWorld.jpg",
-    CloseNormal_png : "asset/CloseNormal.png",
-    CloseSelected_png : "asset/CloseSelected.png",
-    MapGeral_png : "asset/MapGeral.png",
-    MapGeralBloqueado1_png : "asset/MapGeralBloqueado1.png",
-    MapGeralBloqueado2_png : "asset/MapGeralBloqueado2.png",
-    MapGeralBloqueado3_png : "asset/MapGeralBloqueado3.png",
-    Mini_mapa1_png : "asset/Mini_mapa1.png",
-    Mini_mapa2_png : "asset/Mini_mapa2.png",
-    Mini_mapa3_png : "asset/Mini_mapa3.png",
-    Mini_mapa4_png : "asset/Mini_mapa4.png",
-    unidade01_png : "asset/unidade01.png",
-    unidade02_png : "asset/unidade02.png",
-    unidade03_png : "asset/unidade03.png",
-    unidade04_png : "asset/unidade04.png"
+    HelloWorld_png : "asset/mapa/HelloWorld.jpg",
+    CloseNormal_png : "asset/mapa/CloseNormal.png",
+    CloseSelected_png : "asset/mapa/CloseSelected.png",
+    MapGeral_png : "asset/mapa/MapGeral.png",
+    MapGeralBloqueado1_png : "asset/mapa/MapGeralBloqueado1.png",
+    MapGeralBloqueado2_png : "asset/mapa/MapGeralBloqueado2.png",
+    MapGeralBloqueado3_png : "asset/mapa/MapGeralBloqueado3.png",
+    Mini_mapa1_png : "asset/mapa/Mini_mapa1.png",
+    Mini_mapa2_png : "asset/mapa/Mini_mapa2.png",
+    Mini_mapa3_png : "asset/mapa/Mini_mapa3.png",
+    Mini_mapa4_png : "asset/mapa/Mini_mapa4.png",
+    unidade01_png : "asset/mapa/unidade01.png",
+    unidade02_png : "asset/mapa/unidade02.png",
+    unidade03_png : "asset/mapa/unidade03.png",
+    unidade04_png : "asset/mapa/unidade04.png"
     
 };
 
