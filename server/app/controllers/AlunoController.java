7024e64105b595eb68d8cb430132057ee35de0d4
brunomaarques@hotmail.com.br <brunomaarques@hotmail.com.br>
2016-06-03 22:20:30 -0300

    Correção bug do json na autenticacao 2

--- server/app/controllers/AlunoController.java
+++ server/app/controllers/AlunoController.java
@@ -140,7 +140,7 @@ public class AlunoController extends Controller {
 				Aluno a = AlunoDatabase.selectAlunoByEmail(email);
 		
 				if (a != null && a.getStatus() != Constantes.STATUS_REMOVIDO) {
-					if(isNovaSessao && a.isLogado() && !a.getSessao().equals(sessao)){
+					if(!isNovaSessao && a.isLogado() && !a.getSessao().equals(sessao)){
 						return ok(AdminJson.getMensagem("Você já está logado em outro dispositivo. Deseja iniciar uma nova sessão para este dispositivo?"));
 					}
 					if(a.getStatus() == Constantes.STATUS_ATIVO){
